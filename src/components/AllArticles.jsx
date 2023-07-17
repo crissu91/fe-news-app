c7c80ebae81eeafe5027c333a9a2966d242ae7f6
crissu91 <cristinaf.stefan@gmail.com>
2023-07-17 14:59:33 +0100

    small changes to article component

--- src/components/AllArticles.jsx
+++ src/components/AllArticles.jsx
@@ -11,21 +11,21 @@ function AllArticles() {
     },[])
 
 return (
-    <section>
+    <main>
             {articles.map((article)=>{
                 return (
-                    <div key={article.article_id} className="articles">
+                    <section key={article.article_id} className="articles">
                         <h2>{article.title}</h2>
-                        <img width="500px" src={`${article.article_img_url}`} />
+                        <img width="500px" src={`${article.article_img_url}`} alt={`image reflecting the ${article.title}`}/>
                         <p>Author: {article.author}</p>
                         <p>Created at: {article.created_at}</p>
                         <p>Votes: {article.votes}</p>
                         <p>Comments: {article.comment_count}</p>
-                        <p></p>Topic: {article.topic}
-                    </div>
+                        <p>Topic: {article.topic}</p>
+                    </section>
                 )
             })}
-    </section>
+    </main>
     )
 }
 
