599965fc8009d77c2f7bd6c1a7752c4da2a99fcd
crissu91 <cristinaf.stefan@gmail.com>
2023-07-18 11:42:56 +0100

    create new component for comments

---
+++ src/components/CommentsByArticleId.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { getCommentsByArticleId } from "../api";
+import { useParams } from "react-router";
+
+function CommentsByArticleId() {
+    const {article_id} = useParams()
+    const [comments, setComments] = useState([])
+
+    useEffect(()=>{
+        getCommentsByArticleId(article_id)
+        .then((res)=>{
+            setComments(res)
+        })
+        .catch(console.log)
+    }, [])
+
+    return (
+        <div className="comment">
+            <h2 className="comments-header">Comments:</h2>
+            <ul>{comments.map(({comment_id, body, author, created_at, votes}) => (
+                <li key={comment_id}>
+                    <p className="comment-body">{body}</p>
+                    <p className="comment-author">Author: {author}</p>
+                    <p className="comment-created-at">Created at: {created_at}</p>
+                    <p className="comment-votes">Votes: {votes}</p>
+                </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+export default CommentsByArticleId;
