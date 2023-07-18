69a354d18d1432ebce974e5777d7a3a8d52b7dd5
crissu91 <cristinaf.stefan@gmail.com>
2023-07-18 14:40:48 +0100

    add article votes functionality

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react"
-import { getArticleById } from "../api"
+import { getArticleById, patchArticleById } from "../api"
 import { useParams } from "react-router"
+import Error from "./Error"
 
 function SingleArticle() {
     const {article_id} = useParams()
     const [article, setArticle] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [articleVotes, setArticleVotes] = useState(0)
+    const [apiError, setApiError] = useState(null)
+
+    const formattedDate = new Date(article.created_at).toLocaleString();
 
     useEffect(()=>{
         getArticleById(article_id)
@@ -13,12 +18,36 @@ function SingleArticle() {
             setArticle(res[0])
             setIsLoading(false)
         })
-        .catch(console.log)
+        .catch((err) =>{
+            setApiError(err)
+        })
     },[])
 
+    
+    function handleClick() {
+        setArticleVotes((currentArticleVotes=>{
+            return currentArticleVotes + 1;
+        }))
+        patchArticleById(article_id, 1).catch((err)=>{
+            setApiError(err)
+            setArticleVotes((currentArticleVotes=>{
+                return currentArticleVotes - 1;
+            }))
+        })
+    }
+    
     if (isLoading) {
         return <p>Loading...</p>
     }
+
+    if (apiError) {
+        return (
+            <Error 
+                errorStatus={apiError.response.status}
+                errorMessage={apiError.response.data.msg}
+            />
+        )
+    } 
 
     return (
         <div>
@@ -26,11 +55,12 @@ function SingleArticle() {
                 <img className="article-img-single" src={`${article.article_img_url}`} alt={`image reflecting the ${article.title}`}/>
             <ul key={article.article_id} className="single-article">
                 <li>Author: {article.author}</li>
-                <li>Created at: {article.created_at}</li>
-                <li>Votes: {article.votes}</li>
+                <li>Created at: {formattedDate}</li>
                 <li>Comments: {article.comment_count}</li>
                 <li>Topic: {article.topic}</li>
+                <li>Votes: {article.votes + articleVotes}</li>
             </ul>
+                <button aria-label="vote this comment" onClick={handleClick}>Vote article</button>
                 <p>{article.body}</p>
         </div>
             )
