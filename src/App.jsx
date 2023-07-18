cecfd76e6da753f733199ffd1e732888573bada8
crissu91 <cristinaf.stefan@gmail.com>
2023-07-18 10:19:54 +0100

    add home page component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router'
 import AllArticles from './components/AllArticles'
 import Header from './components/Header'
 import SingleArticle from './components/SingleArticle'
+import HomePage from './components/HomePage'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <div className="app">
       <Header />
       <Routes>
+        <Route path='/' element={<HomePage />} />
         <Route path='/api/articles' element={<AllArticles />} />
         <Route path='/api/articles/:article_id' element={<SingleArticle />} />
       </Routes>
