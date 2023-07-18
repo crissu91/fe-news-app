2bf88e7d6a7e3781213f96043894494dad9bcba8
crissu91 <cristinaf.stefan@gmail.com>
2023-07-18 08:24:52 +0100

    add new component for single article path

---
+++ src/components/SingleArticle.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+import { getArticleById } from "../api"
+import { useParams } from "react-router"
+
+function SingleArticle() {
+    const {article_id} = useParams()
+    const [article, setArticle] = useState([])
+
+    useEffect(()=>{
+        getArticleById(article_id)
+        .then((res) =>{
+            setArticle(res[0])
+        })
+    },[])
+
+    return (
+        <div>
+                <h2 className="article-title">{article.title}</h2>
+                <img className="article-img-single" src={`${article.article_img_url}`} alt={`image reflecting the ${article.title}`}/>
+            <ul key={article.article_id} className="single-article">
+                <li>Author: {article.author}</li>
+                <li>Created at: {article.created_at}</li>
+                <li>Votes: {article.votes}</li>
+                <li>Comments: {article.comment_count}</li>
+                <li>Topic: {article.topic}</li>
+            </ul>
+                <p>{article.body}</p>
+        </div>
+            )
+    }
+
+export default SingleArticle;
