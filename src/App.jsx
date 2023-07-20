d0e257ca376335396e0f2d2b34e061966772503b
crissu91 <cristinaf.stefan@gmail.com>
2023-07-20 10:12:41 +0100

    add app files

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AllArticles from './components/AllArticles'
 import Header from './components/Header'
 import SingleArticle from './components/SingleArticle'
 import HomePage from './components/HomePage'
+import AllTopics from './components/AllTopics'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path='/api/articles' element={<AllArticles />} />
         <Route path='/api/articles/:article_id' element={<SingleArticle />} />
         <Route path='*' element={<Error errorStatus={404} errorMessage={'Not found: Page does not exist'}/>}/>
+        <Route path='/api/topics' element={<AllTopics />} />
       </Routes>
     </div>
   )
