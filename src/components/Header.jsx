8399bfb5e79313445aff41e915cfac77f901d8ad
crissu91 <cristinaf.stefan@gmail.com>
2023-07-21 15:48:51 +0100

    add title for home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ function Header() {
     return (
         <section className="header">
             <img src="https://www.manchesterdigital.com/storage/6766/Northcoders-Primary-Logo---Red.png" alt="northcoders logo in colour red" />
+            <h1>News</h1>
         </section>
     )
 }
